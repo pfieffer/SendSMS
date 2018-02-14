554c56122b92eca8a05a5bc4c51926d8a7416e91
pfieffer <cbpmgr@gmail.com>
2018-02-14 18:30:35 +0545

    Put your nexmo credentials here

---
+++ app/src/main/java/np/com/ravipun/sendsms/Config.java
@@ -0,0 +1,11 @@
+package np.com.ravipun.sendsms;
+
+/**
+ * Created by ravi on 2/14/2018.
+ */
+
+public class Config{
+    //Obtain these at https://www.nexmo.com/
+    public static final String ApiKey = "";
+    public static final String ApiSecret = "";
+}
